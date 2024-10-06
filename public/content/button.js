96445104712954b0f09f1b63a878d8810fc73423
chan8632 <duscks8632@gmail.com>
2024-10-06 18:50:10 +0900

    갑자기 버튼이 사라짐

--- public/content/button.js
+++ public/content/button.js
@@ -1,8 +1,25 @@
+// import axios from "axios";
+
 // 1. 버튼 요소 생성
+const clickTransButton = async (e) => {
+  e.preventDefault();
+  const originText = document.getElementById("body").innerText;
+  console.log(originText);
+  // try {
+  //   const response = await axios.post("back", {
+  //     originText,
+  //   });
+  //   if (response.status === 200) {
+  //     console.log("서버로 잘감");
+  //   }
+  // } catch (error) {
+  //   console.log(error);
+  // }
+};
 const button = document.createElement("button");
 
 // 2. 버튼에 텍스트 추가
-button.innerText = "Click Me";
+button.innerText = "번역하기";
 
 // 3. 버튼 스타일 설정
 button.style.position = "fixed"; // 고정 위치 설정
@@ -11,9 +28,11 @@ button.style.right = "20px"; // 오른쪽에서 20px 왼쪽으로
 button.style.padding = "10px 20px"; // 버튼 크기 설정
 button.style.backgroundColor = "skyblue";
 button.style.zIndex = "999999";
+button.style.border = "round";
 // 4. 버튼 이벤트 리스너 추가
 button.addEventListener("click", function () {
-  alert("버튼이 클릭되었습니다!");
+  clickTransButton;
+  alert("di");
 });
 
 // 5. 버튼을 body에 추가
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
+import axios from "axios";
+import { useState } from "react";
+import icon from "/icons/icon_origin.png";
+
 const App = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [faillogin, setFailLogin] = useState(false);
+
+  const onSubmit = async (e) => {
+    e.preventDefault(); // 오타 수정
+
+    try {
+      // 아이디와 비번 서버에 보내기
+      const response = await axios.post("/login", null, {
+        params: {
+          username: id,
+          password: password,
+        },
+      });
+      // 위 post에 대한 응답으로 토큰 수령
+      if (response.status === 200) {
+        localStorage.setItem("accessToken", response.headers.access);
+        localStorage.setItem("idName", id);
+      }
+    } catch (error) {
+      console.log(error);
+      setFailLogin(true);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <input
-          type="text"
-          placeholder="아이디"
-          className="bg-slate-400"
-        ></input>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center gap-3">
+      <img src={icon} alt="main icon" className="w-1/12"></img>
+      <div className="text-xl font-semibold">웹페이지 번역</div>
+      <form onSubmit={onSubmit} className="flex flex-col gap-3">
+        <div className="">
+          <input
+            type="text"
+            id="id"
+            placeholder="  아이디"
+            className="border-2 border-black rounded-sm bg-slate-50"
+            onChange={setId}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="password"
+            id="password"
+            placeholder="  비밀번호"
+            className="border-2 border-black rounded-sm bg-slate-50"
+            onChange={setPassword}
+          ></input>
+        </div>
+      </form>
+      <button type="submit" className="w-1/4">
+        로그인
+      </button>
+      {faillogin && <div>아이디나 비번이 잘못되었습니다</div>}
+    </div>
   );
 };
 
